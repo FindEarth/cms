a3d69e38d72d7acede79b2838858aed5b1403033
Matias Tucci <matiastucci@gmail.com>
2017-03-27 02:59:14 -0400

    person edit

---
+++ src/components/main/person/PersonEdit.vue
@@ -0,0 +1,128 @@
+<script>
+  import personService from 'services/person';
+
+  import BasicInformation from 'components/main/person/create-steps/BasicInformation';
+  import Geo      from 'components/main/person/create-steps/Geolocation';
+  import FileUpload       from 'components/main/person/create-steps/FileUpload';
+  import Resume           from 'components/main/person/create-steps/Resume';
+
+  export default {
+    name: 'PersonEdit',
+
+    components: { BasicInformation, Geo, FileUpload, Resume },
+
+    data() {
+      return {
+        step     : 1,
+        isLoading: false,
+
+        person: {
+          name       : '',
+          age        : '',
+          description: {},
+          gender     : 'M',
+          lastSeenAt : '',
+          geo: {
+            loc        : [],
+            address    : '',
+            city       : '',
+            postalCode : '',
+            countryCode: '',
+            country    : ''
+          },
+          contacts: [{ name: '', phone: '', email: '' }],
+          photos  : []
+        }
+      };
+    },
+
+    mounted() {
+      this.isLoading = true;
+      this.getPerson(this.$route.params.personId);
+    },
+
+    methods: {
+      getPerson(personId) {
+        personService.getById(personId)
+          .then((person) => {
+            this.person    = person;
+            this.isLoading = false;
+          });
+      },
+
+      onBasicInformationSubmitted(basicInformation) {
+        Object.assign(this.person, basicInformation);
+        this.step += 1;
+      },
+
+      onGelocationSubmitted(geolocation) {
+        Object.assign(this.person.geo, geolocation);
+        this.step += 1;
+      },
+
+      onFileUploadSubmitted(files) {
+        this.person.photos = files;
+        this.step += 1;
+      },
+
+      onResumeSubmitted() {
+        this.isLoading = true;
+        return personService.update(this.person)
+          .then(() => this.$router.push({ name: 'person-list' }));
+      }
+    }
+  };
+</script>
+
+<template lang='pug'>
+  div(v-loading='isLoading', element-loading-text='Creando persona')
+    el-steps.steps(:space='150', :active='step')
+      el-step(title='Información básica', icon='information')
+      el-step(title='Geolocalización', icon='search')
+      el-step(title='Carga de fotos', icon='picture')
+      el-step.last-step(title='Resumen', icon='check')
+
+    el-card.box-card
+      basic-information(
+        v-if='step === 1',
+        :person='person',
+        v-on:basic-information-submitted='onBasicInformationSubmitted'
+      )
+
+      geo(
+        v-if='step === 2',
+        :geo='person.geo',
+        v-on:gelocation-submitted='onGelocationSubmitted'
+      )
+
+      file-upload(
+        v-if='step === 3',
+        :files='person.photos',
+        v-on:file-upload-submitted='onFileUploadSubmitted'
+      )
+
+      resume(
+        v-if='step === 4',
+        :person='person',
+        :isEdit='true',
+        v-on:resume-submitted='onResumeSubmitted'
+      )
+</template>
+
+<style lang='scss' scoped>
+  .steps {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 30px;
+
+    .last-step {
+      width: auto !important;
+    }
+  }
+
+  .box-card {
+    padding: 20px 20px 0 20px;
+  }
+</style>
