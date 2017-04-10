bccaa6e6a11c92d02ed7129f38ef18f7a35641c9
Matias Tucci <matiastucci@gmail.com>
2017-04-09 23:04:29 -0400

    fix person request list

--- src/components/main/person-request/PersonRequestList.vue
+++ src/components/main/person-request/PersonRequestList.vue
@@ -33,7 +33,7 @@
       },
 
       deletePersonRequest(index, personRequest) {
-        const message = 'Esta operacion borrara la solicitud de persona de' +
+        const message = 'Esta operacion borrara la solicitud de persona de ' +
                         `${personRequest.name} permanentemente, Desea continuar?`;
         this.$confirm(message, 'Eliminar Solicitud de Persona', {
           confirmButtonText: 'OK',
@@ -54,28 +54,23 @@
 <template lang='pug'>
   el-table(
     :data='personRequests',
+    v-on:row-click='onSelect',
     border='',
     style='width: 100%',
     v-loading='isLoading',
     element-loading-text='Cargando solicitudes',
     empty-text='No se ha encontrado solicitudes de carga de personas'
   )
-    el-table-column(fixed='', prop='name', label='Nombre', width='200')
-    el-table-column(prop='age', label='Edad', width='90')
-    el-table-column(prop='gender', label='Genero', width='90')
-    el-table-column(prop='geo.city', label='Ciudad', width='260')
-    el-table-column(prop='geo.countryCode', label='Pais', width='70')
-    el-table-column(prop='geo.postalCode', label='Zip', width='80')
-    el-table-column(prop='createdAt', label='Fecha de Creación', width='210')
-    el-table-column(fixed='right', label='Acciones', width='120')
+    el-table-column(fixed='', prop='name', label='Nombre', width='200', class-name='pointer')
+    el-table-column(prop='age', label='Edad', width='90', class-name='pointer')
+    el-table-column(prop='gender', label='Genero', width='90', class-name='pointer')
+    el-table-column(prop='geo.city', label='Ciudad', width='260', class-name='pointer')
+    el-table-column(prop='geo.countryCode', label='Pais', width='70', class-name='pointer')
+    el-table-column(prop='geo.postalCode', label='Zip', width='80', class-name='pointer')
+    el-table-column(prop='createdAt', label='Fecha de Creación', width='210', class-name='pointer')
+    el-table-column(fixed='right', label='Acciones', width='120', class-name='pointer')
       template(scope='scope')
           el-button-group
-            el-button(
-              type='primary',
-              size='mini',
-              icon='edit'
-              @click.native.prevent='onSelect'
-              )
             el-button(
               type='primary',
               size='mini',
