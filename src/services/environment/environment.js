3732b12d54c0d268b5f9bedd51c14a164ae0daee
ndelvalle <nicolas.delvalle@gmail.com>
2017-04-19 20:31:28 -0400

    Create environment service and change npm and node engine

---
+++ src/services/environment/environment.js
@@ -0,0 +1,12 @@
+export default class {
+  constructor(env = {}) {
+
+    if (!env.name) { throw new Error('env.name is required'); }
+    if (!env.url) { throw new Error('env.url is required'); }
+    if (!env.startsWith) { throw new Error('env.startsWith is required'); }
+
+    this.name       = env.name;
+    this.url        = env.url;
+    this.startsWith = env.startsWith;
+  }
+}
